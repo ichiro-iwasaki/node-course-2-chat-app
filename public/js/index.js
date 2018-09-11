a1e7c2adfe571a7b3d545b4da40bf3281dfb1437
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-09-11 14:42:25 +0900

    add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Andrew',
+    text: 'Yup, that works for me.'
+  });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
+
