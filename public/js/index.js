5344cae20b862139efec3d29972acf31f4e19b6b
ichiro-iwasaki <iwasaki0302prog@gmail.com>
2018-09-11 14:52:33 +0900

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', () => {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', () => {
